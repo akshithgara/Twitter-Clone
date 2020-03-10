42f727d662a72e72af517b79e71784648c6b8b85
Akshith Gara <akshithgara@Akshiths-MacBook-Pro.local>
2020-03-09 23:18:19 -0500

    Added validatipn for tweets in server [ag]

--- server/index.js
+++ server/index.js
@@ -10,8 +10,32 @@ app.get('/', (req,res) => {
   })
 });
 
+function isValidTweet(tweet) {
+  if (tweet.name && tweet.name.toString().trim() != ''
+      && tweet.tweet && tweet.tweet.toString().trim() != ''){
+        return true;
+      }
+      else{
+        return false;
+      }
+}
+
+
+
 app.post('/tweet', (req,res)=> {
-    console.log(req.body);
+    if (isValidTweet(req.body)) {
+      const tweet = {
+        name: req.body.name.toString(),
+        tweet: req.body.tweet.toString()
+      };
+      console.log(tweet)
+    } else{
+      res.status(422);
+      res.json({
+        message: 'How hard is tweeting a legit tweet? 😂'
+      });
+      console.log('How hard is tweeting a legit tweet? 😂')
+    }
 });
 
 app.listen(5000, () =>{
